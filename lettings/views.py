457ef026c8bdc78bee2d1cce2f62d13106e2cd9b
NZT48DEV <nzt48.dev@gmail.com>
2026-01-29 14:40:46 +0100

    Modif en cours

---
+++ lettings/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Letting
+
+def index(request):
+    lettings_list = Letting.objects.all()
+    context = {"lettings_list": lettings_list}
+    return render(request, "lettings/index.html", context)
+
+def letting(request, letting_id):
+    letting_obj = get_object_or_404(Letting, id=letting_id)
+    context = {
+        "title": letting_obj.title,
+        "address": letting_obj.address,
+    }
+    return render(request, "lettings/letting.html", context)
